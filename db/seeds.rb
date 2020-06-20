0e8b9b8a4c984fbc5f5a9ad8ddbffc1a1840f24e
Иван Окулов <i.okulov@fun-box.ru>
2020-06-20 13:37:20 +0300

    conf redis

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Dir[Rails.root.join('db/seeds/*.rb')].sort.each { |file| require file }
 
 Seeds::Users.create!
-#Seeds::News.create!
+Seeds::News.create!
 Seeds::VaccinationPlaces.create!
 Seeds::Pages.create!
