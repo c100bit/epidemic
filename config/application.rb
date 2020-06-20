5f2b2f35d24e8c16b90558a3fe1c50ac19d6ad45
Иван Окулов <i.okulov@fun-box.ru>
2020-06-20 18:19:59 +0300

    adding landing

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,7 @@ module Epidemic
     config.i18n.default_locale = :ru
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.assets.paths << Rails.root.join('app/assets/fonts')
 
   end
 end
